fc80a87799693963af993eb252df172ed55a37a4
Massimo Siani <maxsiani@gmail.com>
2019-12-03 15:36:58 +0100

    add the shift functions

--- src/main/scala/coscala/types/Strip.scala
+++ src/main/scala/coscala/types/Strip.scala
@@ -3,10 +3,22 @@ package coscala.types
 import cats.Comonad
 
 case class Strip[A](as: Seq[A], index: Int) {
+  assert(index >= 0)
+  assert(index < as.length)
 
   def extract: A = as(index)
 
   def coflatMap[B](f: Strip[A] => B): Strip[B] = Strip(as.indices.map(i => f(Strip(as, i))), index)
+
+  def shiftLeft: Strip[A] = {
+    val newIndex = index - 1
+    Strip(as, if (newIndex < 0) as.length - 1 else newIndex)
+  }
+
+  def shiftRight: Strip[A] = {
+    val newIndex = index + 1
+    Strip(as, if (newIndex >= as.length) 0 else newIndex)
+  }
 }
 
 object Strip {
