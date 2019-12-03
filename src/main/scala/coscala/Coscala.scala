6a24cb7ea50ecb5142d151a1f01791ef3740f90b
Massimo Siani <maxsiani@gmail.com>
2019-12-03 21:36:39 +0100

    the working game

--- src/main/scala/coscala/Coscala.scala
+++ src/main/scala/coscala/Coscala.scala
@@ -1,6 +1,47 @@
 package coscala
 
+import cats.implicits._
+import cats.data.{State, Store}
+import coscala.types.{Pairing, Strip}
+import Pairing.stateStorePairing
+
 object Coscala extends App {
 
-  
+  implicit private class CoscalaOps(s: Strip[Boolean]) {
+    def w30: Strip[Boolean] = s.coflatMap { s1 =>
+      val left = s1.shiftLeft.extract
+      val middle = s1.extract
+      val right = s1.shiftRight.extract
+      (left && !middle && !right) || (!left && middle && right) || (!left && middle && !right) || (!left && !middle && right)
+    }
+
+    def render: String = s.as.map(if (_) "X" else " ").mkString
+  }
+
+  private def stripStore(start: Strip[Boolean]): Store[List[Strip[Boolean]], String] =
+    Store(
+      state => state.map(strip => CoscalaOps(strip).render).mkString("\n"),
+      List(start)
+    )
+
+  private val size = 30
+  private val iterations = 90
+  private val start: Strip[Boolean] = Strip(
+    ((0 until size).map(_ => false) :+ true) ++ (0 until size).map(_ => false),
+    0
+  )
+
+  private def actions(steps: Int): State[List[Strip[Boolean]], Unit] = {
+    if (steps == 0) State.pure(())
+    else for {
+      ls <- State.get
+      _ <- State.modify((list: List[Strip[Boolean]]) => list ++ List(ls.last.w30))
+      result <- actions(steps - 1)
+    } yield result
+  }
+
+  private val actualStripStore: Store[List[Strip[Boolean]], String] = stripStore(start)
+
+  private val solution = Pairing.select(actions(iterations))(actualStripStore.coflatMap(identity))
+  println(solution.extract)
 }
--- src/main/scala/coscala/types/Pairing.scala
+++ src/main/scala/coscala/types/Pairing.scala
@@ -1,8 +1,30 @@
 package coscala.types
 
+import cats.implicits._
+import cats.data.{State, Store}
 import cats.{Comonad, Monad}
 
-trait Pairing {
+abstract class Pairing[M[_]: Monad, W[_]: Comonad] {
+  def pair[A, B, C](f: A => B => C): M[A] => W[B] => C
+}
 
-  def pair[A, B, C, M[_] : Monad, W[_] : Comonad](f: A => B => C): M[A] => W[B] => C
+object Pairing {
+
+  type StateType = List[Strip[Boolean]]
+  type StateBool[A] = State[StateType, A]
+  type StoreBool[A] = Store[StateType, A]
+
+  implicit def stateStorePairing[A]: Pairing[StateBool, StoreBool] = new StateStorePairing
+
+  def select[A, B, M[_]: Monad, W[_]: Comonad](mb: M[B])(wwa: W[W[A]])(implicit p: Pairing[M, W]): W[A] =
+    p.pair[B, W[A], W[A]](_ => identity)(mb)(wwa)
+
+  private[types] class StateStorePairing extends Pairing[StateBool, StoreBool] {
+    override def pair[A, B, C](f: A => B => C): StateBool[A] => StoreBool[B] => C =
+      state =>
+        store => {
+          val (next, value) = state.run(store.index).value
+          f(value)(store.fa(next))
+        }
+  }
 }
